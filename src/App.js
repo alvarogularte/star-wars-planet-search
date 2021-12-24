137c0d0d19c131dea1bea87dc429e363546a2734
alvarogularte <alvaro.gularte@gmail.com>
2021-12-24 11:16:11 -0300

    Few changes at style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PlanetsProvider from './providers/PlanetsProvider';
+import Header from './components/Header';
 import Table from './components/Table';
 import FilterInputs from './components/FilterInputs';
 import ColumnFilter from './components/ColumnFilter';
@@ -13,6 +14,7 @@ import './App.css';
 function App() {
   return (
     <PlanetsProvider>
+      <Header />
       <FilterInputs />
       <ColumnFilter />
       <ComparisonFilter />
